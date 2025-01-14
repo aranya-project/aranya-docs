262fef81b74964a7de8775bc3f48eacbd38f26e0
Scott McKeon <smckeon@spideroak.com>
2025-01-14 14:55:23 -0800

    Body should not jump when menu is opened

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,23 @@
-(function(document) {
-  var toggle = document.querySelector('.sidebar-toggle');
-  var sidebar = document.querySelector('#sidebar');
-  var checkbox = document.querySelector('#sidebar-checkbox');
+(function (document) {
+  var toggle = document.querySelector(".sidebar-toggle");
+  var sidebar = document.querySelector("#sidebar");
+  var checkbox = document.querySelector("#sidebar-checkbox");
 
-  document.addEventListener('click', function(e) {
-    var target = e.target;
-
-    if(!checkbox.checked ||
-       sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
-
-    checkbox.checked = false;
-  }, false);
+  document.addEventListener(
+    "click",
+    function (e) {
+      var target = e.target;
+      if (sidebar.contains(target) || target === checkbox) {
+        // Do nothing is clicking in the sidebar.
+        return;
+      } else if (target === toggle) {
+        // Toggle menu when menu toggle clicked.
+        checkbox.checked = !checkbox.checked;
+      } else if (checkbox.checked) {
+        // Close the menu if the document body is clicked.
+        checkbox.checked = false;
+      }
+    },
+    false
+  );
 })(document);
